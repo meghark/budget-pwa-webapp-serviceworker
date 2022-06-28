a6ad0f4358abe8ebbde85452e0bfb285691da118
meghark <98137756+meghark@users.noreply.github.com>
2022-06-27 23:05:45 -0700

    Add logic to server requests from cache or server

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,3 +43,25 @@ self.addEventListener('activate', function(e) {
     })
   );
 });
+
+//Service worker will intercept all fetch calls.
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+
+  //Check if resource exists in cache, if so respond from cache
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) 
+      { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } 
+      else 
+      {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+    })
+  )
+})
