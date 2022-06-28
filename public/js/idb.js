62c154adcab8c712e40006fa8dda64f0bfa8eee9
meghark <98137756+meghark@users.noreply.github.com>
2022-06-27 20:12:04 -0700

    Added data saving in offline mode

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,7 +21,7 @@ request.onsuccess = function(event) {
   
     // check if app is online, send all budget and withdrawl information
     if (navigator.onLine) {
-        //uploadTransaction();
+        uploadTransaction();
     }
   };
 
@@ -29,3 +29,60 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // When there is a connection failure and a new transaction is created following code will be executed.
+function saveRecord(record) {
+    // Open a connection to indexed db.
+    const transaction = db.transaction(['transaction'], 'readwrite');
+  
+    // access the object store within the db
+    const budgetObjectStore = transaction.objectStore('transaction');
+  
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+  }
+
+  function uploadTransaction()
+  {
+      // connection to db
+        const transaction = db.transaction(['transaction'], 'readwrite');
+
+        // access your object store
+        const transactionObjectStore = transaction.objectStore('transaction');
+
+        // read all data in the store
+        const getAll = transactionObjectStore.getAll();
+
+        // getAll() is an async function, once successful execute the following
+         getAll.onsuccess = function() {
+        // If db is not empty send data to api. result holds all saved data.
+        if (getAll.result.length > 0) {
+                fetch('/api/transaction', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                
+                //Once saved to server remove all currently saved records.
+                const transaction = db.transaction(['transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('transaction');
+                transactionObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+  }
+
+
+//event listener when network connection    is back on
+  window.addEventListener('online', uploadTransaction);
